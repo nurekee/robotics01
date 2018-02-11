1a7209d4bf9b36f441b6518f5f65db053ddf9290
Nurekee <nurekee@MacBook-Air-Nurekee.local>
2018-02-11 21:47:06 +0600

    initial

--- index.js
+++ index.js
@@ -1,5 +1,79 @@
 const TekegramBot = require('node-tlegram-api')
 const TOKEN = "474187863:AAE0ObKhCeHGx_MNRZpxLv055L_mPN1cC4M\n"
 const bot = new TekegramBot(TOKEN,{polling:true})
+const KB = {
+  menu:'Меню',
+  zhoba:'Жоба жайында',
+    oku:'Bilgen Robotics оқушыға не береді ?',
+    artyk:'Bilgen Robotics-тың артықшылықтары:',
+    maksat:'Bilgen Robotics-тың алға қойған мақсаттары:',
+    prins:'Bilgen Robotics-тың негізгі принциптері',
+    baga:'Bilgen Robotics курстарының бағасы:',
+    sony:'Cұрақтар бойынша',
+    back:'Кері қайту'
+}
+bot.onText(/\/start/,msg => {
+  const text =  `Сәлем , ${msg.form.first_name},\n Bilgen Robotics жобасына қош келдің!!!`
 
-bot.on('message',msg.chat.id,`Сәлем ,${msg.form.first_name} \n, Bilgen Robotics жобасына қош келдің!!! )
+    })
+
+    bot.on('message',msg =>{
+      switch (msg.text) {
+          case KB.zhoba:
+            sendPictureScreen(msg.chat.id)
+            break
+          case KB.oku:
+            break
+          case KB.artyk:
+            break
+          case KB.maksat:
+            break
+          case KB.prins:
+            break
+          case KB.baga:
+            break
+          case KB.sony:
+            break
+          case KB.back:
+              bot.sendMessage(msg.chat.id,text,{
+
+                  reply_markup:{
+                      keyboard: [
+                          [ KB.zhoba, KB.oku ,KB.artyk, KB.maksat, KB.prins,KB.baga,KB.sony]
+                      ]
+                  } break
+
+      }
+    })
+
+
+})
+
+
+function snedPictureScreen(chatId) {
+
+  bot.sendMessage(chatId,`Жоба жайында`,
+      {
+        reply_markup:{
+          keyboard:[
+              [KB.zhoba,KB.oku,KB.artyk,KB.maksat,KB.prins,KB.baga,KB.sony],
+              [KB.back]
+          ]
+        }
+      })
+
+    
+}
+function sendGreeting(chatId,sayHello = true) {
+
+    bot.sendMessage(msg.chat.id,text,{
+
+        reply_markup:{
+            keyboard: [
+                [ //KB.zhoba,KB.oku,KB.artyk,KB.maksat,KB.prins,KB.baga,KB.sony]
+                    KB.menu]
+            ]
+        }
+
+
+    }
